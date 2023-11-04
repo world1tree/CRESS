a513b4548d9763685bef69ec5768d45dd1bfb15f
zaiheshi <zaiheshi@gmail.com>
2023-11-04 19:00:35 +0800

    dynamic

--- cress/criterions/speech_and_text_translation_criterion.py
+++ cress/criterions/speech_and_text_translation_criterion.py
@@ -88,6 +88,91 @@ class SpeechAndTextTranslationCriterion(LabelSmoothedCrossEntropyCriterion):
         loss, _ = self.compute_loss(model, text_output, sample, reduce=reduce)
         return loss
 
+    def compute_target_distribute_of_x_cross_s_no_grad(self, model, sample):
+        text_audio_input = {
+            "audio": sample["net_input"]["audio"],
+            "audio_lengths": sample["net_input"]["audio_lengths"],
+            "source": sample["net_input"]["source"],
+        }
+        x_cross_s_encoder_out = model.encoder.forward_x_cross_s(**text_audio_input)
+        # decoder此时是没有梯度的
+        with torch.no_grad():
+            prev_output_tokens = sample["net_input"]["prev_output_tokens"]
+            decoder_out = model.decoder(
+                prev_output_tokens=prev_output_tokens, encoder_out=x_cross_s_encoder_out
+            )
+            lprobs = model.get_normalized_probs(decoder_out, log_probs=True)
+            target = model.get_targets(sample, decoder_out)
+        return lprobs, target, x_cross_s_encoder_out
+
+    def compute_target_distribute_of_x_no_grad(self, model, sample):
+        text_input = {
+            "src_tokens": sample["net_input"]["source"],
+            "src_lengths": sample["net_input"]["source_lengths"],
+            "mode": "mt",
+        }
+        mt_encoder_output = model.encoder(**text_input)
+        with torch.no_grad():
+            prev_output_tokens = sample["net_input"]["prev_output_tokens"]
+            decoder_out = model.decoder(
+                prev_output_tokens=prev_output_tokens, encoder_out=mt_encoder_output
+            )
+            lprobs = model.get_normalized_probs(decoder_out, log_probs=True)
+            target = model.get_targets(sample, decoder_out)
+        return lprobs, target, mt_encoder_output
+
+    def forward_x_cross_s_mix_auto(self, model, sample, reduce):
+        # target1与target2应该相同
+        y1_lprobs, target1, x_cross_s_encoder = self.compute_target_distribute_of_x_cross_s_no_grad(model, sample)
+        y2_lprobs, target2, x_encoder = self.compute_target_distribute_of_x_no_grad(model, sample)
+        p1_lprobs = y1_lprobs.gather(dim=-1, index=target1.unsqueeze(-1))
+        p2_lprobs = y2_lprobs.gather(dim=-1, index=target2.unsqueeze(-1))
+        # B, T
+        p1_lprobs = p1_lprobs.squeeze(-1)
+        p2_lprobs = p2_lprobs.squeeze(-1)
+        # mask
+        pad_mask1 = target1.eq(self.padding_idx)
+        p1_lprobs.masked_fill_(pad_mask1, 0.0)
+        pad_mask2 = target2.eq(self.padding_idx)
+        p2_lprobs.masked_fill_(pad_mask2, 0.0)
+        # sum: p1表示参考语音的，p2表示没有参考语音
+        p1_sum_lprobs = p1_lprobs.sum(dim=-1)
+        p2_sum_lprobs = p2_lprobs.sum(dim=-1)
+        # 0表示选择参考语音，1表示选择参考文本
+        label = torch.argmax(torch.stack([p1_sum_lprobs, p2_sum_lprobs]), dim=0)
+        selected_index = label.bool().detach()
+
+        encoder_out_attend_s = x_cross_s_encoder["encoder_out"][0].transpose(0, 1)
+        # get basic shape
+        bsz, text_len, emb_dim = encoder_out_attend_s.size()
+        # x_cross_s is better
+        bsz_x_cross_s = torch.sum(~selected_index).float()
+        encoder_out_padding_mask = x_cross_s_encoder["encoder_padding_mask"][0]
+        encoder_embedding = x_cross_s_encoder["encoder_embedding"][0]
+
+        encoder_out_origin = x_encoder["encoder_out"][0].transpose(0, 1)
+
+        mix_encoder_out = torch.zeros((bsz, text_len, emb_dim), device=encoder_out_origin.device)
+        mix_encoder_out[selected_index] = encoder_out_origin[selected_index]
+        mix_encoder_out[~selected_index] = encoder_out_attend_s[~selected_index]
+        mix_encoder_out = mix_encoder_out.transpose(0, 1)
+
+        encoder_out = {
+            "encoder_out": [mix_encoder_out],  # T x B x C
+            "encoder_padding_mask": [encoder_out_padding_mask],  # B x T
+            "encoder_embedding": [encoder_embedding],  # B x T x C
+            "encoder_states": [],  # List[T x B x C]
+            "src_tokens": [],
+            "src_lengths": [],
+        }
+
+        prev_output_tokens = sample["net_input"]["prev_output_tokens"]
+        decoder_out = model.decoder(
+            prev_output_tokens=prev_output_tokens, encoder_out=encoder_out
+        )
+        loss, _, lprobs, target = self.compute_loss_with_lprobs(model, decoder_out, sample, reduce=reduce)
+        return loss, lprobs, target, bsz_x_cross_s
+
     def forward_x_cross_s_mix(self, model, sample, reduce):
         # x_cross_s
         text_audio_input = {
@@ -169,6 +254,7 @@ class SpeechAndTextTranslationCriterion(LabelSmoothedCrossEntropyCriterion):
         st_loss, mt_loss, ext_mt_loss = torch.Tensor([0]).cuda(), torch.Tensor([0]).cuda(), torch.Tensor([0]).cuda()
         jsd_loss = torch.Tensor([0]).cuda()
         st_size, mt_size, ext_mt_size = 0, 0, 0
+        bsz_x_cross_s = 0
 
         mode = sample["net_input"]["mode"]
         if mode == "st":
@@ -176,7 +262,7 @@ class SpeechAndTextTranslationCriterion(LabelSmoothedCrossEntropyCriterion):
             if self.mt_finetune and self.training:
                 st_loss, st_lprobs, st_target = self.forward_st(model, sample, reduce)
                 # mt_loss = self.forward_mt(model, sample, reduce)
-                mt_loss, x_cross_s_lprobs, mt_target = self.forward_x_cross_s_mix(model, sample, reduce)
+                mt_loss, x_cross_s_lprobs, mt_target, bsz_x_cross_s = self.forward_x_cross_s_mix_auto(model, sample, reduce)
                 jsd_loss = self.compute_jsd_loss(st_lprobs, x_cross_s_lprobs, st_target, mt_target, self.padding_idx)
                 loss = st_loss + mt_loss + jsd_loss
                 st_size = mt_size = sample_size = sample["ntokens"]
@@ -200,6 +286,7 @@ class SpeechAndTextTranslationCriterion(LabelSmoothedCrossEntropyCriterion):
             "ext_mt_sample_size": ext_mt_size,
             "ntokens": sample["ntokens"],
             "nsentences": sample["target"].size(0),
+            "bsz_x_cross_s": bsz_x_cross_s.data,
             "sample_size": sample_size,
         }
         
@@ -213,6 +300,7 @@ class SpeechAndTextTranslationCriterion(LabelSmoothedCrossEntropyCriterion):
         mt_loss_sum = sum(log.get("mt_loss", 0) for log in logging_outputs)
         ext_mt_loss_sum = sum(log.get("ext_mt_loss", 0) for log in logging_outputs)
         sample_size = sum(log.get("sample_size", 0) for log in logging_outputs)
+        bsz_x_cross_s = sum(log.get("bsz_x_cross_s", 0) for log in logging_outputs)
         st_sample_size = sum(log.get("st_sample_size", 0) for log in logging_outputs)
         mt_sample_size = sum(log.get("mt_sample_size", 0) for log in logging_outputs)
         ext_mt_sample_size = sum(log.get("ext_mt_sample_size", 0) for log in logging_outputs)
@@ -232,6 +320,9 @@ class SpeechAndTextTranslationCriterion(LabelSmoothedCrossEntropyCriterion):
         )
         metrics.log_scalar(
             "ext_mt_loss", ext_mt_loss_sum / ext_mt_sample_size / math.log(2) if ext_mt_sample_size != 0 else 0, ext_mt_sample_size, round=3
+        )
+        metrics.log_scalar(
+            "bsz_x_cross_s", bsz_x_cross_s / mt_sample_size if mt_sample_size != 0 else 0, mt_sample_size, round=3
         )
 
     @staticmethod
